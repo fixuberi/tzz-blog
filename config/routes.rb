724809cfbe22c5c344f81b438650498330c4e426
fixuberi <comeclarity007@gmail.com>
2018-07-07 11:38:26 +0300

    sign in\out in process

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via:'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
