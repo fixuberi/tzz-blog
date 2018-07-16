464376667c6ec61966ba1dd927850c8f427c8202
fixuberi <comeclarity007@gmail.com>
2018-07-16 20:36:01 +0300

    creating posts done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,8 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:avatar) }
+  it { should respond_to(:posts)}
+  it { should respond_to(:feed) }
 
   describe "when name is not present" do
     before { @user.name = " " }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,6 @@
 
 def valid_signin(user)
+  visit signin_path
   fill_in 'Email',    with: user.email.upcase
   fill_in 'Password', with: user.password
   click_button "Sign in"
